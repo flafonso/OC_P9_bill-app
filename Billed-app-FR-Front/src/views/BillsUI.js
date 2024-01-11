ee6d2ed0f914db2ed2a804855269cbb1783ebced
Flavien Afonso <af.flavien@gmail.com>
2024-01-11 14:44:40 +0100

    fixed the bug displaying bills in the wrong order, moidif in containers/Bills.js views/BillUI.js __tests__/Bills.js

--- Billed-app-FR-Front/src/views/BillsUI.js
+++ Billed-app-FR-Front/src/views/BillsUI.js
@@ -1,6 +1,7 @@
 import VerticalLayout from './VerticalLayout.js'
 import ErrorPage from "./ErrorPage.js"
 import LoadingPage from "./LoadingPage.js"
+import { formatDate } from "../app/format.js"
 
 import Actions from './Actions.js'
 
@@ -9,7 +10,7 @@ const row = (bill) => {
     <tr>
       <td>${bill.type}</td>
       <td>${bill.name}</td>
-      <td>${bill.date}</td>
+      <td>${formatDate(bill.date)}</td>
       <td>${bill.amount} €</td>
       <td>${bill.status}</td>
       <td>
@@ -20,7 +21,9 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  return (data && data.length) ? data.sort((a, b) => {
+    return new Date(b.date) - new Date(a.date)
+  }).map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
